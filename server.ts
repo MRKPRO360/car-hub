9858046732c7c7025916e581c2911fdf9cdca444
Rezaul <mdrezaulkarrim@gmail.com>
2024-11-21 23:02:24 +0600

    db set successfully

--- server.ts
+++ server.ts
@@ -1,6 +1,23 @@
+import mongoose from 'mongoose';
 import app from './app';
 import config from './src/app/index';
 
-app.listen(config.port, () => {
-  console.log(`App is running on ${config.port}`);
-});
+const DB = config.database_url?.replace(
+  '<db_password>',
+  config.database_password as string
+);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB as string);
+
+    app.listen(config.port, () => {
+      console.log(`App is running on ${config.port}`);
+    });
+  } catch (err: any) {
+    console.error(`Erorr: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,7 @@ dotenv.config({
 });
 
 export default {
-  database_url: '',
-  database_password: '',
+  database_password: process.env.DATABASE_PASSWORD,
+  database_url: process.env.DATABASE_URL,
   port: process.env.PORT,
 };
